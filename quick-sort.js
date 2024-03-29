1d47cc967d2c9d58e1417b50b47ad76c1c4d0f7f
Стасік Петрмк <stasps210@gmail.com>
2024-03-29 10:32:27 +0200

    update

--- quick-sort.js
+++ quick-sort.js
@@ -3,7 +3,26 @@ let count = 0
 
 
 function quickSort(array) {
-  
+  if (array.length <= 1) {
+    return array
+  }
+  let pivotIndex = Math.floor(array.length / 2)
+  let pivot = array[pivotIndex]
+  let less = []
+  let greater = []
+  for (i = 0; i < array.length; i++) {
+    count++
+    if (i === pivotIndex) {
+      continue
+    }
+    if (array[i] < pivot) {
+      less.push(array[i])
+    } else {
+      greater.push(array[i])
+    }
+  }
+  return [...quickSort(less), pivot, ...quickSort(greater)]
 }
 
+console.log(quickSort(arr))
 console.log(count)
